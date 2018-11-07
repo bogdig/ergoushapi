43d8771857f0a5f38f492dbb939bf8bf0994b8a4
sunergou <shz1000@126.com>
2018-11-07 19:17:22 +0800

    add gateway

--- service-gateway/src/main/java/com/ergou/servicegateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/ergou/servicegateway/ServiceGatewayApplication.java
@@ -2,11 +2,30 @@ package com.ergou.servicegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServiceGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //basic proxy
+                .route(r -> r.path("/baidu")
+                        .uri("http://baidu.com:80/")
+                ).build();
+    }
+
+//    @RequestMapping("/fallback")
+//    public Mono<String> fallBack() {
+//        return Mono.just("fallback");
+//    }
+
+
 }
